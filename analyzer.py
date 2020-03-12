2c00374988d681e2b46522bf1e0a52602139ea51
MichaelHsu171 <jing.xu@intel.com>
2020-03-12 10:04:47 +0900

    Added functionality to process mkldnn v0.x verbose messages

--- analyzer.py
+++ analyzer.py
@@ -31,6 +31,28 @@ class DNNL_Verbose:
         try:
             with open(self.args.logfile) as f:
                 for line in f:
+                    if line.startswith('mkldnn_verbose'):
+                        line = line.replace('\r', '')
+                        line = line.replace('\n', '')
+                        c = line.split(',')
+                        if c[1] == 'info':
+                            if c[2].startswith('Intel'):
+                                self.version = c[2]
+                            if c[2].startswith('Detected ISA'):
+                                self.cpu_isa = ','.join(c[2:]).replace('Detected ISA is ', '')
+                        else:
+                            op = Op()
+                            op.type = c[1]
+                            op.device = 'cpu'
+                            op.name = c[2]
+                            op.implementation = c[3]
+                            op.propagation = c[4]
+                            op.iodata = c[5]
+                            op.aux = c[6]
+                            op.reserved = ''
+                            op.benchdnn = c[7]
+                            op.time = c[8]
+                            self.ops.append(op)
                     if line.startswith('dnnl_verbose,'):
                         line = line.replace('\r', '')
                         line = line.replace('\n', '')
@@ -65,20 +87,32 @@ class DNNL_Verbose:
         except IOError:
             print('{} not accessible'.format(self.args.logfile))
 
+    def print_sequence(self, print_create=False):
+        for op in self.ops:
+            if not print_create and op.type.startswith('create'):
+                continue
+            print('{},{},{},{},{}'.format(op.type, op.name, op.device, op.benchdnn, op.time))
+
     def getOpList(self):
         return {op.name for op in self.ops}
 
     def analyze_exec(self):
         ops_time = {}
+        ops_num = {}
         for op in self.getOpList():
             time = 0
+            num = 0
             for op1 in list(filter(lambda x: x.name == op and x.type == 'exec', self.ops)):
                 time += float(op1.time)
+                num += 1
                 # print('{}: {}, {}, {}, {}'.format(op1.name, op1.device, op1.iodata, op1.benchdnn, op1.time))
             ops_time[op] = time
+            ops_num[op] = num
         ops_time = sorted(ops_time.items(), key=lambda item:item[1], reverse=True)
         # ops_time = collections.OrderedDict(ops_time)
-        print(ops_time)
+        print('\nOperations Summary')
+        for time in ops_time:
+            print('{}: {:.2f}ms, {} time(s)'.format(time[0], time[1], ops_num[time[0]]))
 
 def main():
     parser = argparse.ArgumentParser()
@@ -86,12 +120,13 @@ def main():
     args = parser.parse_args()
     dv = DNNL_Verbose(args)
     dv.load()
-    print(dv.version)
-    print(dv.cpu_runtime)
-    print(dv.cpu_isa)
-    print(dv.gpu_runtime)
-    print(dv.gpu_engine)
+    print('version:     {}'.format(dv.version))
+    print('cpu_runtime: {}'.format(dv.cpu_runtime))
+    print('cpu_isa:     {}'.format(dv.cpu_isa))
+    print('gpu_runtime: {}'.format(dv.gpu_runtime))
+    print('gpu_engine:  {}'.format(dv.gpu_engine))
     dv.analyze_exec()
+    # dv.print_sequence()
 
 if __name__ == '__main__':
     main()
